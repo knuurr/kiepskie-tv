6ce7f6bfea53ad593250571575e472d629d565bd
Knurr <knurr@example.com>
2024-05-10 05:23:01 +0200

    Add prettier

--- src/routes/DashedBox.svelte
+++ src/routes/DashedBox.svelte
@@ -1,41 +1,33 @@
 <script>
-    // export let heading;
-    // export let content;
-	import { Container, Col, Row } from '@sveltestrap/sveltestrap';
+</script>
 
-  </script>
-  
-  <style>
-	#dashed-box  {
-	  /* width: auto; */
-	  min-height: 200px;
-	  display: flex;
-	flex-direction: column;
-	  justify-content: center;
-	  align-items: center;
-	  /* color: var(--); */
-	  /* font-size: 16px; */
-	  /* background-color: #000; */
-	  border: 2px dashed var(--txt-clr);
-	  border-radius: 5px;
+<div id="dashed-box">
+  <slot />
+</div>
+
+<style>
+  #dashed-box {
+    /* width: auto; */
+    min-height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* color: var(--); */
+    /* font-size: 16px; */
+    /* background-color: #000; */
+    border: 2px dashed var(--txt-clr);
+    border-radius: 5px;
     margin-top: 40px;
     margin-bottom: 40px;
-	padding-top: 10px;
-	padding-bottom: 10px;
-	padding-left: 8px;
-	padding-right: 8px;
-	}
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 8px;
+    padding-right: 8px;
+  }
 
-	#dashed-box > * {
-		margin-top: 1.5rem;
-		margin-bottom: 1.5rem;
-	}
-
-
-  </style>
-  
-  <div id="dashed-box">
-		<slot />
-    
-  </div>
-  
+  /* #dashed-box > * { */
+  /* margin-top: 1.5rem; */
+  /* margin-bottom: 1.5rem; */
+  /* } */
+</style>
