ecb38c05c450522a64b5430fb6ea5a93ef7c5525
Knurr <knurr@example.com>
2024-06-04 17:24:35 +0200

    Implemented cop to Clipboard for Toasty

--- src/routes/toasty/+page.svelte
+++ src/routes/toasty/+page.svelte
@@ -14,8 +14,25 @@
   let currentToast = "";
   let previousToast = "";
   let isRolling = false;
+  let isSuccess = false;
 
   let toasts = [];
+
+  // Function to copy content to clipboard
+  function copyToClipboard() {
+    navigator.clipboard
+      .writeText(currentToast)
+      .then(() => {
+        console.log("Text copied to clipboard");
+        isSuccess = true;
+        setTimeout(() => {
+          isSuccess = false;
+        }, 3000); // Reset after 3 seconds
+      })
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
+      });
+  }
 
   function getRandomToast() {
     // console.log('getRandomToast() CALLED');
@@ -87,6 +104,46 @@
       </div>
 
       <div class="text-center mt-4">
+        {#if currentToast !== ""}
+          <button
+            on:click={copyToClipboard}
+            class="btn btn-block {isSuccess ? 'btn-success' : 'btn-info'} mb-1"
+            disabled={isRolling ? true : false}
+          >
+            <h4>{isSuccess ? "Skopiowano!" : "Kopiuj do schowka"}</h4>
+            {#if isSuccess}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+            {:else}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+                />
+              </svg>
+            {/if}
+          </button>
+        {/if}
         <button
           on:click={getRandomToast}
           class="btn btn-primary btn-warning btn-block bg-yellow-500"
